51a70e56473542302d24cb6cebeff1b736b2027f
lucas <lucasmacielaguiar@gmail.com>
2021-10-13 19:28:19 -0300

    Trying use code in heroku

--- Dockerfile
+++ Dockerfile
@@ -13,3 +13,7 @@ RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key
     sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' &&\
     apt-get -y update &&\
     apt-get install -y google-chrome-stable
+
+
+# Heroku
+CMD ./scripts/start_prod.sh
---
+++ scripts/start_prod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo '========== CHECKING FOR UNINSTALLED PKGs AND INSTALLING'
+pip install -r requirements.txt
+
+echo '========== CHANGE TO API DIR'
+cd pcts_scrapers_api
+
+echo '========== MAKING MIGRATIONS'
+python manage.py makemigrations
+
+echo '========== RUNNING MIGRATIONS'
+python manage.py migrate
+
+echo '========== ENSURING ADMIN USER'
+python manage.py ensure_adminuser --username=$DJANGO_SUPERUSER_USERNAME \
+    --email=$DJANGO_SUPERUSER_EMAIL \
+    --password=$DJANGO_SUPERUSER_PASSWORD
+
+echo '========== RUNNING SERVER'
+python manage.py runserver 0.0.0.0:$PORT
