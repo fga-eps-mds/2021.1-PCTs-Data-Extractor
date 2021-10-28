d72c5da91cde8c29dbc3a0860a79989fcad22d07
Lucas Silva <lucas.gomesgs0@gmail.com>
2021-10-27 21:07:49 -0300

    Created test to incra site Added parsed content from incra site

--- pcts_crawlers_scripts/pcts_crawlers/tests/test_generic_crawler.py
+++ pcts_crawlers_scripts/pcts_crawlers/tests/test_generic_crawler.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from ..spiders.generic_crawler import GenericCrawlerSpider
 from .responses.responses import fake_response_from_file
@@ -9,9 +10,7 @@ class GenericCrawlerTest(unittest.TestCase):
     
     def setUp(self):
         self.url = (
-            "https://www.gov.br/incra/pt-br/assuntos/noticias/"
-            "quilombolas-tratam-de-titulacao-e-acesso-a-creditos"
-            "-com-gestores-do-incra-rs"
+            "https://www.gov.br/incra/pt-br/assuntos/noticias/quilombolas-tratam-de-titulacao-e-acesso-a-creditos-com-gestores-do-incra-rs"
         )
         self.crawler = GenericCrawlerSpider(
             url_root=self.url,
@@ -29,16 +28,23 @@ class GenericCrawlerTest(unittest.TestCase):
         
 
     def test_document_page_parse_from_incra(self):
-        results = self.crawler.parse_page(fake_response_from_file('fixtures/incra_scraper/document_page.html', self.url), "test")
+        results = self.crawler.parse_page(fake_response_from_file('fixtures/incra_scraper/document_page.html', self.url), "Quilombolas tratam de titulação e acesso a créditos com gestores do Incra/RS — Português (Brasil)", True)
+        
+        responses_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(responses_dir, 'responses/fixtures/incra_scraper/parsed_content.txt')
+        file = open(file_path, 'r')
+
+        expected_attributes = {
+            "source": "incra",
+            "url": self.url,
+            "title": "Quilombolas tratam de titulação e acesso a créditos com gestores do Incra/RS — Português (Brasil)",
+            "content": file.read()
+        }
+        file.close()
+
         for item in results:
-            print("===============================START=================================")
-            print(item)
-            print("================================================================")
-            print(item['source'])
-            print("================================================================")
-            print(item['url'])
-            print("================================================================")
-            print(item['title'])
-            print("================================================================")
-            print(item['content'])
-            print("================================================================")
+            self.assertEquals(expected_attributes['source'], item['source'])
+            self.assertEquals(expected_attributes['url'], item['url'])
+            self.assertEquals(expected_attributes['title'], item['title'])
+            self.assertIsNotNone(item['content'])
+            self.assertEquals(expected_attributes['content'], item['content'])
