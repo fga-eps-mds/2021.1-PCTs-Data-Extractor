732a67bed60b685b34fe0845c092a6b65e5f44fd
Lucas Silva <lucas.gomesgs0@gmail.com>
2021-11-01 01:13:31 -0300

    Refactored execution model class

--- pcts_crawlers_api/crawlers/tests/test_crawlers_model.py
+++ pcts_crawlers_api/crawlers/tests/test_crawlers_model.py
@@ -96,6 +96,7 @@ class TestCrawlerExecutionGroupModel(TestCase):
         self.crawler_execution_group.task_name = "incra_crawler_group"
         self.crawler_execution_group.finish_datetime = datetime(2021, 10, 14, 8, 34, 56)
         self.crawler_execution_group.state = SUCCESS
+        self.crawler_execution_group.save()
 
         updated_crawler_execution_group = CrawlerExecutionGroup.objects.filter(id=self.crawler_execution_group.id).get()
         self.assertEqual(updated_crawler_execution_group, self.crawler_execution_group)
@@ -110,25 +111,38 @@ class TestCrawlerExecutionGroupModel(TestCase):
 
 
 class TestCrawlerExecutionModel(TestCase):
-
-    def test_document_creation(self):
-        crawler_model = Crawler.objects.create(
+    def setUp(self):
+        self.crawler_model = Crawler.objects.create(
             site_name="mpf",
             url_root="www.mpf.mp.br",
             task_name="mpf_crawler",
         )
 
-        crawler_execution_group_model = CrawlerExecutionGroup.objects.create(
-            crawler=crawler_model,
+        self.crawler_execution_group_model = CrawlerExecutionGroup.objects.create(
+            crawler=self.crawler_model,
             task_name="mpf_crawler_group",
             finish_datetime=datetime(2021, 10, 10, 8, 35, 21),
             state=STARTED,
         )
 
-        crawler_execution_group = crawler_execution_group_model
+        self.crawler_execution = CrawlerExecution.objects.create(
+            crawler_execution_group=self.crawler_execution_group_model,
+            task_id="ec7a5f20-314f-11ec-8d3d-0242ac130003",
+            task_name="mpf_crawler_keyword",
+            finish_datetime=datetime(2021, 10, 10, 8, 40, 00),
+            keyword="quilombolas",
+            state=FAILURE,
+            crawled_pages=40,
+            saved_records=37,
+            dropped_records=3,
+            error_log="error",
+        )
+
+    def test_document_creation(self):
+        crawler_execution_group = self.crawler_execution_group_model
         task_id = "ec7a5f20-314f-11ec-8d3d-0242ac130003"
         task_name = "mpf_crawler_keyword"
-        finish_datetime = datetime.now()
+        finish_datetime = datetime(2021, 10, 10, 8, 40, 00)
         keyword = "quilombolas"
         state = FAILURE
         crawled_pages = 40
@@ -136,28 +150,15 @@ class TestCrawlerExecutionModel(TestCase):
         dropped_records = 3
         error_log = "error"
 
-        crawler_execution = CrawlerExecution.objects.create(
-            crawler_execution_group=crawler_execution_group,
-            task_id=task_id,
-            task_name=task_name,
-            finish_datetime=finish_datetime,
-            keyword=keyword,
-            state=state,
-            crawled_pages=crawled_pages,
-            saved_records=saved_records,
-            dropped_records=dropped_records,
-            error_log=error_log,
-        )
-
         self.assertEqual(crawler_execution_group.id,
-                         crawler_execution.crawler_execution_group.id)
-        self.assertEqual(task_id, crawler_execution.task_id)
-        self.assertEqual(task_name, crawler_execution.task_name)
-        self.assertEqual(finish_datetime, crawler_execution.finish_datetime)
-        self.assertEqual(keyword, crawler_execution.keyword)
-        self.assertEqual(state, crawler_execution.state)
-        self.assertEqual(crawled_pages, crawler_execution.crawled_pages)
-        self.assertEqual(saved_records, crawler_execution.saved_records)
-        self.assertEqual(dropped_records, crawler_execution.dropped_records)
-        self.assertEqual(error_log, crawler_execution.error_log)
-        self.assertIsNotNone(crawler_execution.start_datetime)
+                         self.crawler_execution.crawler_execution_group.id)
+        self.assertEqual(task_id, self.crawler_execution.task_id)
+        self.assertEqual(task_name, self.crawler_execution.task_name)
+        self.assertEqual(finish_datetime, self.crawler_execution.finish_datetime)
+        self.assertEqual(keyword, self.crawler_execution.keyword)
+        self.assertEqual(state, self.crawler_execution.state)
+        self.assertEqual(crawled_pages, self.crawler_execution.crawled_pages)
+        self.assertEqual(saved_records, self.crawler_execution.saved_records)
+        self.assertEqual(dropped_records, self.crawler_execution.dropped_records)
+        self.assertEqual(error_log, self.crawler_execution.error_log)
+        self.assertIsNotNone(self.crawler_execution.start_datetime)
